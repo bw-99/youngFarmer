98d3e92954288d176082cae8a2ac8de2ce813dc1
ByungWoo <63662808+bw-99@users.noreply.github.com>
2022-10-05 03:50:51 +0900

    Add: LoginPage UI

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -11,11 +11,70 @@ const Background = styled.div`
     background-repeat: no-repeat;
 `
 
-const MainText = styled.span`
-    font-family: TTOmniGothicUL;
+const MainBox = styled.div`
+    display: flex;
+    height: 100vh;
+    flex-direction: column;
+    justify-content: space-between;
+`
+
+const MainTextBox = styled.div`
+    padding: 193px 122px 0px 24px;
+`
+
+const MainTextBold = styled.span`
+    font-family: SCDream;
     font-size: 44px;
+    font-weight: ExtraBold;
     line-height: 59px;
     color: #ffffff;
+`
+
+const MainTextLight = styled.span`
+    font-family: SCDream;
+    font-size: 44px;
+    font-weight: light;
+    line-height: 59px;
+    color: #ffffff;
+`
+
+const BottomBox = styled.div`
+  width: 100vw;
+  height: 262px;
+  border-radius: 16px;
+  background-color: #ffffff;
+`
+
+
+const KakaoBox = styled.div`
+  margin: 20px 16px 12px 16px;
+  height: 50px;
+  border-radius: 8px;
+  background-color: #fde500;
+`
+
+const NaverBox = styled.div`
+  margin: 12px 16px 12px 16px;
+  height: 50px;
+  border-radius: 8px;
+  background-color: #00c73c;
+`
+
+const AppleBox = styled.div`
+  margin: 12px 16px 12px 16px;
+  height: 50px;
+  border-radius: 8px;
+  background-color: #000000;
+`
+
+const LookAroundBeforeLogin = styled.div`
+  font-family: AppleSDGothicNeo;
+  padding: 16px 115px 15px 115px;
+  border-radius: 8px;
+  font-size: 15px;
+  font-weight: 500;
+  color: #777777;
+  width: 100vw;
 `
 
 function LoginPage(props: any) {
@@ -39,26 +98,34 @@ function LoginPage(props: any) {
 
     return (
         <div style={{backgroundImage: `url(${bg})`, backgroundRepeat: "no-repeat", width: "100vw", height:"100vh", backgroundSize: "cover"}}>
-            <div>
-                <div>
-                    <MainText>
-                        올 가을엔
-                    </MainText>
-                </div>
-                <div>
-                    <MainText>
-                        청송사과
-                    </MainText>
-                    <MainText>
-                        로
-                    </MainText>
-                </div>
-                <div>
-                    <MainText>
-                        비타민 충전
-                    </MainText>
-                </div>
-            </div>
+            <MainBox>
+                <MainTextBox>
+                    <div>
+                        <MainTextLight>
+                            올 가을엔
+                        </MainTextLight>
+                    </div>
+                    <div>
+                        <MainTextBold>
+                            청송사과
+                        </MainTextBold>
+                        <MainTextLight>
+                            로
+                        </MainTextLight>
+                    </div>
+                    <div>
+                        <MainTextLight>
+                            비타민 충전
+                        </MainTextLight>
+                    </div>
+                </MainTextBox>
+                <BottomBox>
+                    <KakaoBox> 카카오로 계속하기 </KakaoBox>
+                    <NaverBox> 네이버로 계속하기 </NaverBox>
+                    <AppleBox> Apple로 계속하기 </AppleBox>
+                    <LookAroundBeforeLogin> 로그인 전 둘러보기 </LookAroundBeforeLogin>
+                </BottomBox>
+            </MainBox>
         </div>
 
     );
