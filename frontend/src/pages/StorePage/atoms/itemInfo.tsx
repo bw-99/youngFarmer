47cc0c068c8cc9991b06fd7038c31ebffa0e0533
kmeee777 <kmeee777@gmail.com>
2022-10-19 20:45:43 +0900

    Refactor: storePage

--- frontend/src/pages/StorePage/atoms/itemInfo.tsx
+++ frontend/src/pages/StorePage/atoms/itemInfo.tsx
@@ -1,120 +1,82 @@
 import styled from "styled-components";
 import React from "react";
 
-export const FarmerProfile = styled.img`
-  width: 33px;
-  height: 33px;
+export const FarmerProfileImage = styled.img`
+  width: 66px;
+  height: 66px;
   border-radius: 33px;
+  /*object-fit: contain;*/
 `
 
 export const FarmerNickname = styled.div`
-  font-family: AppleSDGothicNeo;
-  font-size: 16px;
-  font-weight: 600;
-  color: #979797;
-`
-
-export const FarmerArrow = styled.img`
-  width: 14px;
-  height: 14px;
-  object-fit: contain;
-`
-
-export const ItemTitle = styled.div`
+  width: 77px;
+  height: 27px;
   font-family: AppleSDGothicNeo;
   font-size: 22px;
   font-weight: bold;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: normal;
   color: #272727;
 `
 
-export const ItemLike = styled.img`
-  width: 30px;
-  height: 30px;
-  object-fit: contain;
-`
-
-export const ItemLikeBg = styled.div`
-  width: 48px;
-  height: 48px;
-  background-color: #f5f5f5;
-  border-radius: 50%;
-`
-
-
-export const ItemRateStar = styled.img`
+export const FarmerRateStar = styled.img`
   width: 16px;
   height: 16px;
 `
 
-export const ItemRateText = styled.div`
+export const FarmerRateText = styled.div`
   font-family: AppleSDGothicNeo;
   font-size: 14px;
   color: #444444;
 `
 
-export const ItemRateArrow = styled.img`
-  width: 14px;
-  height: 14px;
-  object-fit: contain;
-`
-
-export const ItemPriceDefault = styled.div`
+export const FarmerIntroduceText = styled.div`
+  margin: 20px 84px 0 0;
   font-family: AppleSDGothicNeo;
-  font-size: 16px;
-  color: #b5b5b5;
-  
-`
-
-export const ItemDiscount = styled.div`
-  font-family: AppleSDGothicNeo;
-  font-size: 24px;
-  font-weight: bold;
-  color: #fb6159;
-`
-
-export const ItemDiscountPrice = styled.div`
-  font-family: AppleSDGothicNeo;
-  font-size: 28px;
-  font-weight: 800;
-  color: #272727;
-`
-
-export const ItemSepLine = styled.div`
-  height: 1px;
-  background-color: #efefef;
-`
-
-export const DeliveryInfoTitle = styled.div`
-  font-family: AppleSDGothicNeo;
-  font-size: 16px;
-  font-weight: bold;
-  color: #444444;
-`
-
-export const DeliveryInfoCategory = styled.div`
-  font-family: AppleSDGothicNeo;
-  width: 70px;
   font-size: 14px;
   font-weight: 500;
-  color: #979797;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: normal;
+  color: #444;
 `
 
-export const DeliveryInfoExplainMain = styled.div`
-  font-family: AppleSDGothicNeo;
-  font-size: 14px;
-  font-weight: 550;
-  color: #6b6b6b;
-`
-
-export const DeliveryInfoExplainSub = styled.div`
-  font-family: AppleSDGothicNeo;
-  font-size: 14px;
-  color: #979797;
-  font-weight: 400;
+export const FarmerSortIcon = styled.div`
+  width: 4px;
+  height: 4px;
+  background-color: #dfdfdf;
+  border-radius: 50%;
   margin-left: 8px;
+  margin-right: 8px;
 `
 
-export const DeliverySepLine = styled.div`
+export const FarmerSortText = styled.div`
+  font-family: AppleSDGothicNeo;
+  font-size: 14px;
+  color: #979797;
+`
+
+export const FarmerFollowButton = styled.div`
+  width: 66px;
+  height: 36px;
+  border-radius: 18px;
+  background-color: #fb6159;
+  font-family: AppleSDGothicNeo;
+  font-size: 13px;
+  font-weight: 500;
+  line-height: 20px;
+  text-align: center;
+  color: #ffffff;
+  display: flex;
+  align-items:center;
+  justify-content: center;
+`
+
+
+export const DetailSepLine = styled.div`
   height: 8px;
   background-color: #f1f1f1;
 `
--- frontend/src/pages/StorePage/atoms/topImage.tsx
+++ frontend/src/pages/StorePage/atoms/topImage.tsx
@@ -20,27 +20,3 @@ width: 100vw;
 top: 0;
 background-image: linear-gradient(to bottom, #000000, rgba(0, 0, 0, 0.88) 31%, rgba(0, 0, 0, 0));
 `
-
-export const BottomBox = styled.div`
-border-radius: 12px;
-background-color: rgba(0, 0, 0, 0.5);
-margin-top: 270px;
-display: flex;
-justify-content: center;
-align-items: center;
-`
-
-export const BottomBoxCurPage = styled.div`
-font-family: AppleSDGothicNeo;
-font-size: 13px;
-font-weight: bold;
-color: #ffffff;
-`
-
-export const BottomBoxTotalPage = styled.div`
-opacity: 0.5;
-font-family: AppleSDGothicNeo;
-font-size: 13px;
-font-weight: 500;
-color: #ffffff;
-`
